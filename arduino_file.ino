de1b70e9d678789f77389323ba8381a34b2d8f72
hunterviolette <hunter@maplevalleypharms.com>
2023-09-17 16:45:54 -0400

    photolab branch

---
+++ arduino_file.ino
@@ -0,0 +1,20 @@
+const int ledPin = 13; 
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(ledPin, OUTPUT);  // Set the LED pin as an output
+  digitalWrite(ledPin, HIGH);  // Turn on the LED
+}
+
+void loop() {
+  int sensorValue = analogRead(A0); 
+  //int sensorValue = random(0, 100);
+
+  Serial.print("[");
+  Serial.print(millis() / 1000);
+  Serial.print(", ");
+  Serial.print(sensorValue);
+  Serial.println("]");
+
+  delay(1000);
+}
