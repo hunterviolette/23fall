e675534c201b4e689551aed39a4f92e790dc3f34
hunterviolette <hunter@maplevalleypharms.com>
2023-10-02 16:21:18 -0400

    c

--- 477/scripts/eco_calcs.py
+++ 477/scripts/eco_calcs.py
@@ -1,37 +1,50 @@
 import pandas as pd
 
+# python -c "from eco_calcs import Profit; Profit.Pharma()"
+# python -c "from eco_calcs import Profit; Profit.Factories()"
 
 class Profit:
-    def Pharma():
+    def Pharma(df: pd.DataFrame = pd.read_csv('tables/pharma.csv')):
 
-        df = pd.read_csv('tables/pharma.csv')
         df["D_profit"] = ((df["Sale Price"] - 20 - 20) * df["Desistothium"]) - 1.2e5
         df["H_profit"] = ((df["Sale Price"] - 20 - 20) * df["Hwaleksium"]) - 1.2e5
         df["S_profit"] = ((df["Sale Price"] - 20 - 20) * df["Schwartzinium"]) - 1.2e5
 
-        print(df)
+        df["D_profit_oil"] = ((df["Sale Price"] - 20 - 14) * df["Desistothium"]) - 1.2e5
+        df["H_profit_oil"] = ((df["Sale Price"] - 20 - 14) * df["Hwaleksium"]) - 1.2e5
+        df["S_profit_oil"] = ((df["Sale Price"] - 20 - 14) * df["Schwartzinium"]) - 1.2e5
 
-        print(df[df["D_profit"] == df["D_profit"].max()][["Sale Price", "D_profit"]], 
-            df[df["H_profit"] == df["H_profit"].max()][["Sale Price", "H_profit"]], 
-            df[df["S_profit"] == df["S_profit"].max()][["Sale Price", "S_profit"]], 
-            sep='\n')
+        Profit.Optimum(df.rename({"Sale Price": "Selling Price"}, axis=1))
         
-    def Factories():
+    def Factories(df: pd.DataFrame = pd.read_csv('tables/factories.csv')):
 
-        df = pd.read_csv('tables/factories.csv')
-        print(df.dtypes) 
-        # Widgets 2 iron, 1 wood
-        # Gadget 1 iron, 3 cloth
-        df["W_profit"] = ((df["Selling Price"] - 20 - 30) * df["Widgets Demand"]) - 1.2e5 
-        df["W_profit_iron"] = ((df["Selling Price"] - 20 - 24) * df["Widgets Demand"]) - 1.2e5
-        df["W_profit_iron_wood"] = ((df["Selling Price"] - 20 - 21) * df["Widgets Demand"]) - 1.2e5
+        df.loc[df["Widgets Demand"] > 2e5, 'Widgets Demand'] == 1e5 # Widgets 2 iron, 1 wood
+        df.loc[df["Gadgets Demand"] > 2e5, 'Gadgets Demand'] == 1e5 # Gadget 1 iron, 3 cloth
+
+        df["W_profit"] = ((df["Selling Price"] - 20 - 30) * df["Widgets Demand"] / 2) - 1.2e5 
+        df["W_profit_iron"] = ((df["Selling Price"] - 20 - 24) * df["Widgets Demand"] / 2 ) - 1.2e5
+        df["W_profit_iron_wood"] = ((df["Selling Price"] - 20 - 21) * df["Widgets Demand"] / 2 ) - 1.2e5
         
         df["G_profit"] = ((df["Selling Price"] - 20 - 40) * df["Gadgets Demand"]) - 1.2e5
         df["G_profit_iron"] = ((df["Selling Price"] - 20 - 31) * df["Gadgets Demand"]) - 1.2e5 
         df["G_profit_iron_cloth"] = ((df["Selling Price"] - 20 - 28) * df["Gadgets Demand"]) - 1.2e5 
 
-        print(df)
+        Profit.Optimum(df)
 
-        print(df[df["W_profit"] == df["W_profit"].max()][["Sale Price", "W_profit"]], 
-              df[df["G_profit"] == df["G_profit"].max()][["Sale Price", "G_profit"]], 
-              sep='\n')
+    @staticmethod
+    def Optimum(df):
+        mdf = pd.DataFrame()
+        for col in df.columns:
+            try:
+                if col.split('_')[1] == 'profit':
+                    d = df[df[col] == df[col].max()][["Selling Price", col]].rename(
+                            {col: 'profit'}, axis=1)
+                    
+                    d["config"] = col
+
+                    mdf = pd.concat([mdf, d], axis=0)
+            except:
+                pass
+
+        print(mdf.sort_values('profit', ascending=False))
+
