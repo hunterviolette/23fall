0235b8c98b47b6799f07ed9c60f827d36ee9fc38
hunterviolette <hunter@maplevalleypharms.com>
2023-12-14 15:07:07 -0500

    c

--- 477/etox_proj/main.py
+++ 477/etox_proj/main.py
@@ -20,53 +20,59 @@ class AnotherProject:
     self.coolingGJ = q(0.354, 'dollar/GJ')
     self.heatingGJ = q(17.7, 'dollar/GJ')
     self.etoxRevenue = q(1.687, 'dollar/kg') * q(44.06, 'kg/kmol')
+    self.ethyCost = (q(332.2, 'kmol/h') * q(28.06, 'kg/kmol') * q(1.49, 'dollar/kg')
+                      ).to('million_dollars/year')
+    self.comd = q(155.786, 'million_dollars/year')
     
+    df["mole flow (kmol/h)"] = 333.2 # Overwrite Aspen Flow
     df["etox flow (kmol/h)"] = df["ETOX mole frac"] * df["mole flow (kmol/h)"]
     df = df.drop(columns=['ETOX mole frac', 'mole flow (kmol/h)'])
 
     def etoxYearly(flowRate):
       return (q(flowRate, 'kmol/h').to('kmol/year') * self.etoxRevenue
-                ).to('million_dollars/year').magnitude.__round__(3)
+                ).to('million_dollars/year').magnitude.__round__(4)
 
     df["Revenue (million_dollars/yr)"] = df['etox flow (kmol/h)'].apply(etoxYearly)
 
     def heatingCost(duty):
       return (q(duty, 'GJ/h').to('GJ/year') * self.heatingGJ
-                ).to('million_dollars/yr').magnitude.__round__(3)
+                ).to('million_dollars/yr').magnitude.__round__(4)
 
     def coolingCost(duty):
       return (q(abs(duty), 'GJ/h').to('GJ/year') * self.coolingGJ
-                ).to('million_dollars/yr').magnitude.__round__(3)
+                ).to('million_dollars/yr').magnitude.__round__(4)
     
     df["heating cost (million_dollars/yr)"] = df['reboiler duty (GJ/h)'].apply(heatingCost)
     df["cooling cost (million_dollars/yr)"] = df['condenser duty (GJ/h)'].apply(coolingCost)
 
     df["Profit (million_dollars/yr)"] = df["Revenue (million_dollars/yr)"] - \
                                         df["heating cost (million_dollars/yr)"] - \
-                                        df["cooling cost (million_dollars/yr)"]
+                                        df["cooling cost (million_dollars/yr)"] - \
+                                        self.comd.magnitude
 
     self.df = df
 
-  def trayProfit(self):
-    fig = go.Figure()
+  def trayFigs(self):
+    df = self.df.loc[self.df["number of stages"] != 90]
+    for x in ["reflux ratio", "feed stage"]:
+      fig = go.Figure()
+      fig.add_trace(go.Scatter(
+                      x=df['number of stages'], 
+                      y=df["Profit (million_dollars/yr)"], 
+                      mode='markers', 
+                      marker=dict(
+                          color=df[x],
+                          colorscale='Jet',
+                          colorbar=dict(title=x)
+                        )
+                    ))
+      
+      fig.update_layout(
+          xaxis_title='Number of trays',
+          yaxis_title='Profit (million dollars/year)',
+      ).show()
 
-    fig.add_trace(go.Scatter(
-                    x=self.df['number of stages'], 
-                    y=self.df["Profit (million_dollars/yr)"], 
-                    mode='markers', 
-                    marker=dict(
-                        color=self.df["reflux ratio"],
-                        colorscale='Jet',
-                        colorbar=dict(title='reflux ratio+')
-                    )
-                  ))
-    
-    fig.update_layout(
-        xaxis_title='Number of trays',
-        yaxis_title='Profit (million dollars/year)',
-    ).show()
-
-  def BestSetups(self):
+  def trayDf(self):
     df, mdf = self.df, pd.DataFrame()
 
     for x in df["number of stages"].unique():
@@ -75,11 +81,42 @@ class AnotherProject:
                       d.loc[(d["Profit (million_dollars/yr)"] == d["Profit (million_dollars/yr)"].max())]
                       ], axis=0)
     
-    print(mdf.drop(columns=['heating cost (million_dollars/yr)', 
-                            "cooling cost (million_dollars/yr)",
-                            "Revenue (million_dollars/yr)"
-                            ]))
+      self.mdf = mdf.drop(columns=[
+                          'heating cost (million_dollars/yr)', 
+                          "cooling cost (million_dollars/yr)",
+                          "Revenue (million_dollars/yr)"
+                        ])
+    
+    md = mdf.loc[mdf["number of stages"] != 90]
+    for x in ["reflux ratio", "feed stage"]:
+      fig = go.Figure()
+      fig.add_trace(go.Scatter(
+                      x=md['number of stages'], 
+                      y=md["Profit (million_dollars/yr)"], 
+                      mode='markers+lines', 
+                      marker=dict(
+                          color=md[x],
+                          colorscale='Jet',
+                          colorbar=dict(title=x.capitalize())
+                        )
+                    ))
+      
+      annotations = [dict(
+        x=x,
+        y=y + 1, 
+        text=str(y),
+        showarrow=False,
+          ) for x, y in zip(md["number of stages"], 
+                            md["Profit (million_dollars/yr)"].round(3))]
+      
+      fig.update_layout(
+          title=f'Optimal Profit for Number of Trays by {x.capitalize()}',
+          xaxis_title='Number of trays',
+          yaxis_title='Profit (million dollars/year)',
+          annotations=annotations
 
+      ).show()
+      
+    print(self.mdf)
 
-
-AnotherProject().BestSetups()
+AnotherProject().trayDf()
